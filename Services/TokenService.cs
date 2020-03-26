1496ba9ec3c00dc8d4aa5436624a708f4667c7dd
Kaique Cabral <kaique.11@hotmail.com>
2020-03-26 23:24:09 +0000

    First Commit - Authorization with Bearer and JWT

---
+++ Services/TokenService.cs
@@ -0,0 +1,28 @@
+﻿using AuthenticationProject.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationProject.Services
+{
+    public class TokenService
+    {
+        public static string GenerateToken(User user)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(Settings.Secret);
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(new Claim[] { 
+                    new Claim(ClaimTypes.Name, user.Username.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString()),
+                }),
+                Expires=DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationProject
+{
+    public class Settings
+    {
+        public static string Secret = "ec0e2603172c73a8b644bb9456c1ff6e";
+    }
+}
